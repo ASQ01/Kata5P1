6258e1b322798454748e334b25ae70a2b679c6c3
ASQ01 <alvarosq2001@gmail.com>
2021-11-14 13:12:18 +0000

    Kata5P1 v2

--- src/Kata5P1.java
+++ src/Kata5P1.java
@@ -8,21 +8,18 @@ import java.sql.SQLException;
 public class Kata5P1 {
     public static void main(String[] args){
         String url = "jdbc:sqlite:KATA5.db";
-        String sql = "SELECT * FROM PEOPLE";
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " direccion text NOT NULL);";
+
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
             Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery(sql);
-
-            while (result.next()){
-                System.out.println(result.getInt("Id") + "\t" +
-                        result.getString("Name") + "\t" +
-                        result.getString("Apellidos") + "\t" +
-                        result.getString("Departamento") + "\t");
-            }
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
         }
-
+        
         catch(SQLException e){
             System.out.println(e.getMessage());
         }
